79fb2572d7365bac460c305f90e246a026e63c02
Al islamiah <bouttari@gmail.com>
2026-07-29 13:33:09 +0100

    Create results.js

---
+++ results.js
@@ -0,0 +1,48 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const results = document.getElementById("results");
+
+async function loadSites() {
+
+  results.innerHTML = "<p>جاري تحميل النتائج...</p>";
+
+  try {
+
+    const querySnapshot = await getDocs(collection(db, "sites"));
+
+    results.innerHTML = "";
+
+    if (querySnapshot.empty) {
+      results.innerHTML = "<p>لا توجد مواقع حاليا.</p>";
+      return;
+    }
+
+    querySnapshot.forEach((doc) => {
+
+      const site = doc.data();
+
+      results.innerHTML += `
+        <div class="result">
+          <h3>${site.title}</h3>
+          <p>${site.description}</p>
+          <a href="${site.url}" target="_blank">${site.url}</a>
+        </div>
+      `;
+
+    });
+
+  } catch (error) {
+
+    console.error(error);
+    results.innerHTML = "<p>حدث خطأ أثناء تحميل النتائج.</p>";
+
+  }
+
+}
+
+loadSites();
